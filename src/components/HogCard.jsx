497845fe8072afd360ce00b89c86e93cf0bb4058
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-06 17:48:47 -0400

    Almost all tests passing. Two tests are failing due to the way react is rendering the content. The behavior of the components is functioning properly so I will leave as is without changing the test suite

--- src/components/HogCard.jsx
+++ src/components/HogCard.jsx
@@ -1,4 +1,4 @@
-// Card for each pig, handles clicks?, details, & hide?
+// Card for each pig, handles clicks, details, & hide
 import React, { useState } from "react";
 import styles from "./styles/HogCard.module.css";
 
@@ -10,30 +10,19 @@ function HogCard({ hog, onHide }) {
 
   return (
     <div aria-label="hog card" className={`ui card ${styles.cardWrapper}`}>
-      <div
-        className={`image ${styles.imageWrapper}`}
-        onClick={handleToggleDetails}
-      >
+      <div className={`image ${styles.imageWrapper}`}>
         <img src={hog.image} alt={`Photo of ${hog.name}`} />
       </div>
 
       <div className={`content ${styles.content}`}>
-        <h3>{hog.name}</h3>
+        <h3 onClick={handleToggleDetails}>{hog.name}</h3>
 
         {showDetails && (
           <div className={styles.details}>
-            <p>
-              <strong>Speciality:</strong> {hog.specialty}
-            </p>
-            <p>
-              <strong>Weight:</strong> {hog.weight}
-            </p>
-            <p>
-              <strong>{hog.greased ? "Greased" : "Nongreased"}</strong>
-            </p>
-            <p>
-              <strong>Medal:</strong> {hog["highest medal achieved"]}
-            </p>
+            <p>{`Specialty: ${hog.specialty}`}</p>
+            <p>{`Weight: ${hog.weight}`}</p>
+            <p>{hog.greased ? "Greased" : "Nongreased"}</p>
+            <p>{`Medal: ${hog["highest medal achieved"]}`}</p>
           </div>
         )}
       </div>
