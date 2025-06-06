1c2ea63615eb52e35a3591ec4a7d65779d5d09f7
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-06 14:39:38 -0400

    Initial App structure in place, ready for component logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import HogForm from "./HogForm";
+import FilterSortBar from "./FilterSortBar";
+import HogList from "./HogList";
 
-import hogs from "../porkers_data";
+import hogsData from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  // State
+  const [hogs, setHogs] = useState(hogsData);
+  const [hidden, setHidden] = useState([]);
+  // Filtering & Sorting State
+  const [showGreasedOnly, setShowGreasedOnly] = useState(false);
+  const [sortBy, setSortBy] = useState("");
+
+  // Add a new hog
+  const addHog = (newHog) => setHogs([...hogs, newHog]);
+
+  // Hide a hog
+  const hideHog = (hogName) => setHidden([...hidden, hogName]);
+
+  // Handle filter change
+  const handleGreasedFilter = () => setShowGreasedOnly((g) => !g);
+
+  // Handle sort change
+  const handleSort = (e) => setSortBy(e.target.value);
+
+  // Prep hogs for display
+  const filteredHogs = hogs
+    .filter((hog) => (showGreasedOnly ? hog.greased : true))
+    .filter((hog) => !hidden.includes(hog.name));
+
+  // Handle sorting
+  let displayHogs = [...filteredHogs];
+  if (sortBy === "name") {
+    displayHogs.sort((a, b) => a.name.localeCompare(b.name)); //TBD
+  } else if (sortBy === "weight") {
+    displayHogs.sort((a, b) => a.weight - b.weight);
+  }
+
+  return (
+    <div className="App">
+      <Nav />
+      <FilterSortBar
+        showGreasedOnly={showGreasedOnly}
+        onFilterChange={handleGreasedFilter}
+        sortBy={sortBy}
+        onSortChange={handleSort}
+      />
+      <HogForm onAdd={addHog} />
+      <HogList hogs={displayHogs} onHide={hideHog} />
+      <footer className="footer subtleText">
+        2025 HogWild | All rights reserved.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
