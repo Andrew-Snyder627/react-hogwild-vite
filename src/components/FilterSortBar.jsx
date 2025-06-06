70ca1fe56b902cd16adff66ca9df957c009c44c8
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-06-06 14:55:03 -0400

    FilterSortBar component built

--- src/components/FilterSortBar.jsx
+++ src/components/FilterSortBar.jsx
@@ -1,1 +1,34 @@
 // Filter logic for pigs
+import React from "react";
+import styles from "./FilterSortBar.module.css";
+
+function FilterSortBar({
+  showGreasedOnly,
+  onFilterChange,
+  sortBy,
+  onSortChange,
+}) {
+  return (
+    <div className="{`us segment ${styles.barWrapper}`}">
+      <label htmlFor="greased" className="{styles.filterLabel}">
+        <input
+          id="greased"
+          type="checkbox"
+          checked={showGreasedOnly}
+          onChange={onFilterChange}
+        />
+        Greased Pigs Only?
+      </label>
+      <label htmlFor="sortBy" className={styles.sortLabel}>
+        Sort by:
+        <select id="sortBy" value={sortBy} onChange={onSortChange}>
+          <option value="">None</option>
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default FilterSortBar;
